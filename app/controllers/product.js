ef6b2a3f33d94d38f2ed33835a536197ddafa612
Matheus Pimentel <matheus200713@gmail.com>
2018-08-23 17:25:52 -0300

    fix

--- app/controllers/product.js
+++ app/controllers/product.js
@@ -12,4 +12,22 @@ module.exports.insert = function(application, req, res) {
 }
 
 module.exports.search = function(application, req, res) {
+  const param = req.params.title
+  console.log('param ->', param)
+  
+  if(param === '') {
+    Product.find({}, function(err, productResponse) {
+      if(err) {
+        return res.status(500).send(err)
+      }
+      return res.send(productResponse)
+    })
+  }
+
+  Product.find({titulo: param}, function(err, productResponse) {
+    if(err) {
+      return res.status(500).send(err)
+    }
+    return res.send(productResponse)
+  })
 }
