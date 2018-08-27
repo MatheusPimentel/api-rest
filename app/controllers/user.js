e6853fecfa00982ffac9fa6121ceabfb97be4843
Matheus Pimentel <matheus200713@gmail.com>
2018-08-27 17:17:18 -0300

    iniciando rotas do usuario

---
+++ app/controllers/user.js
@@ -0,0 +1,14 @@
+module.exports = function (application) {
+  const mapping = '/user'
+  application.put(`${mapping}/alter/:_id`, function (req, res) {
+    application.app.controllers.product.alter(application, req, res)
+  })
+
+  application.delete(`${mapping}/delete/:_id`, function (req, res) {
+    application.app.controllers.product.delete(application, req, res)
+  })
+
+  application.get(`${mapping}/:_id`, function (req, res) {
+    application.app.controllers.product.find(application, req, res)
+  })
+}
