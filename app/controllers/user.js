5760d699d1569531084f20d03794816114823452
Matheus Pimentel <matheus200713@gmail.com>
2018-08-28 15:43:55 -0300

    deletando usuário

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -23,5 +23,10 @@ module.exports.alter = function(application, req, res) {
 
 // FAZ A DELEÇÃO DE UM USUÁRIO
 module.exports.delete = function(application, req, res) {
-  
+  User.deleteOne({_id: req.params._id}, function (err) {
+    if (err) {
+      return res.status(500).send(err)
+    }
+    res.send({message: 'Usuário excluido com sucesso'})
+  })
 }
